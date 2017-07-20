e092eca2c3733bd175543ac22b9d5ab22eee70bb
Heejo Clare Keum <heejo.keum@gmail.com>
2017-07-20 14:38:24 -0400

    fix keywords

--- backend/keywords/app.js
+++ backend/keywords/app.js
@@ -1,44 +1,36 @@
 var request = require("sync-request");
-
 // TODO: params = {...?}
 exports.main = function(params) {
-    var url = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27";
+var url = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27";
 
-    var auth = "Basic OThkMDQxZjktMTgyMy00ZWIwLWIyOTctMjBkYjc0NWM4ZmIzOjAwWGlzb2RhaEpNUQ=="
+var auth = "Basic OThkMDQxZjktMTgyMy00ZWIwLWIyOTctMjBkYjc0NWM4ZmIzOjAwWGlzb2RhaEpNUQ=="
 
-    var headers = {
-        "Content-Type": "application/json",
-        "Authorization": auth
+var headers = {
+    "Content-Type": "application/json",
+    "Authorization": auth
+}
+
+var options = {
+    headers: headers,
+    json: {
+        "text": params.text,
+        'features': params.features
     }
+}
 
-    var options = {
-        headers: headers,
-        json: {
-            "text": "I'm really bored!",
-            'features': {
-                'keywords': {
-                    'emotion': true,
-                    'sentiment': true,
-                    'limit': 10 //limit to 10 keywords.
-                },
-            }
-        } // Change to params.text
-    }
+var res = request("POST", url, options);
+if (res.statusCode >= 300) {
+    return {"success": false};
+}
+// the below will prob need to change
 
-    var res = request("POST", url, options);
-    if (res.statusCode >= 300) {
-        return {"success": false};
-    }
+var list = JSON.parse(res.body.toString("utf8")).keywords
 
-    // the below will prob need to change
-
-    var list = JSON.parse(res.body.toString("utf8")).keywords
-
-    list.sort(function(a, b) {
-        if (a.relevance > b.relevance) return -1;
-        if (a.relevance < b.relevance) return 1;
-        return 0;
-    });
+list.sort(function(a, b) {
+    if (a.relevance > b.relevance) return -1;
+    if (a.relevance < b.relevance) return 1;
+    return 0;
+});
         return {"success":true, "keywords":list};
 }
 
