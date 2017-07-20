d7bc0c69e5f1a06293428e260293b707ee80238a
Heejo Clare Keum <Heejo.Clare.Keum@ibm.com>
2017-07-20 13:44:09 -0400

    keywords implemented

--- backend/keywords/app.js
+++ backend/keywords/app.js
@@ -2,9 +2,9 @@ var request = require("sync-request");
 
 // TODO: params = {...?}
 exports.main = function(params) {
-    var url = ...
+    var url = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27";
 
-    var auth = ...
+    var auth = "Basic OThkMDQxZjktMTgyMy00ZWIwLWIyOTctMjBkYjc0NWM4ZmIzOjAwWGlzb2RhaEpNUQ=="
 
     var headers = {
         "Content-Type": "application/json",
@@ -13,26 +13,32 @@ exports.main = function(params) {
 
     var options = {
         headers: headers,
-        json: { "text": "I've been having a tough day today. Life is just so boring."} // Change to params.text
+        json: {
+            "text": "I'm really bored!",
+            'features': {
+                'keywords': {
+                    'emotion': true,
+                    'sentiment': true,
+                    'limit': 10 //limit to 10 keywords.
+                },
+            }
+        } // Change to params.text
     }
 
     var res = request("POST", url, options);
-
     if (res.statusCode >= 300) {
         return {"success": false};
     }
 
-
     // the below will prob need to change
 
-    var list = JSON.parse(res.body.toString("utf8")).document_tone.tone_categories[0].tones;
+    var list = JSON.parse(res.body.toString("utf8")).keywords
 
     list.sort(function(a, b) {
-        if (a.score > b.score) return -1;
-        if (a.score < b.score) return 1;
+        if (a.relevance > b.relevance) return -1;
+        if (a.relevance < b.relevance) return 1;
         return 0;
-    })
-
-    return {"success":true, "emotions":list};
+    });
+        return {"success":true, "keywords":list};
 }
 
